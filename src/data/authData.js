ed42d34aac997402c6ffda621f1fcbef296f7e0f
YHONIER ARIAS <jhonier18249@gmail.com>
2025-03-13 11:38:55 -0500

    actualizacion

--- src/data/authData.js
+++ src/data/authData.js
@@ -43,6 +43,7 @@ const AuthData = {
                 name: userData.name || userData.email.split('@')[0],
                 role: 'user',
                 is_profile_public: true,
+                gender : userData.gender
             });
 
             await user.save();
