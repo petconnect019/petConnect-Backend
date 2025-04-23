8bc756e0ab0fae61b1a195b78951f9ee087ee68e
YHONIER ARIAS <jhonier18249@gmail.com>
2025-04-23 10:42:26 -0500

    Actualización de las rutas de los endpoints de pago en `paymentRoutes.js`: se cambiaron las rutas de `/response` y `/confirmation` a `/api/payments/response` y `/api/payments/confirmation`, respectivamente. Se añadió un mensaje de consola para confirmar la recepción de pagos.

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const paymentController = require('../controllers/controllerPayment/paymentController');
 
 // Ruta para la respuesta de pago (redirección del usuario)
-router.get('/response', paymentController.paymentResponse);
+router.get('/api/payments/response', paymentController.paymentResponse);
 
 // Ruta para el webhook de ePayco
-router.post('/confirmation', paymentController.confirmPayment);
+router.post('/api/payments/confirmation', paymentController.confirmPayment);
+console.log('Confirmacion de pago recibida');
 
 module.exports = router; 
